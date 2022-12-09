78463d9a094a2a2bb6f066c34d36e872d20d3a8f
Gabriel Goffart <gab.goffart@gmail.com>
2022-12-09 12:29:04 -0500

    use builder in day7 for cleaner interface

--- day7/src/main/kotlin/xyz/ggof/aoc2022/day7/Application.kt
+++ day7/src/main/kotlin/xyz/ggof/aoc2022/day7/Application.kt
@@ -2,62 +2,46 @@ package xyz.ggof.aoc2022.day7
 
 import java.io.File
 
-data class Dir(val name: String, private val entries: MutableList<Dir>) {
-	private var files: Int = 0
+data class Dir(val name: String, val entries: List<Dir> = emptyList(), val files: Int = 0) {
+	val size: Int by lazy { entries.sumOf { it.size } + files }
+	val sizes: List<Int> by lazy { entries.flatMap { it.sizes } + size }
 
-	val size: Int get() = entries.sumOf { it.size } + files
+	fun minOver(value: Int) = sizes.filter { it >= value }.min()
 
-	fun addFile(file: Int) {
-		files += file
+	fun sumUnder(value: Int) = sizes.filter { it < value }.sum()
+	data class Builder(val name: String, val entries: MutableList<Builder> = mutableListOf(), var files: Int = 0) {
+		fun build(): Dir = Dir(name, entries.map { it.build() }, files)
 	}
+}
 
-	fun addDir(dir: Dir) = entries.add(dir)
 
-	fun childWithName(name: String) = entries.first { it.name == name }
+fun List<String>.buildTree(): Dir {
+	val path: MutableList<Dir.Builder> = mutableListOf(Dir.Builder("/"))
 
-	private fun sizes(): List<Int> = entries.flatMap { it.sizes() } + size
-
-	fun minOver(value: Int) = sizes().filter { it >= value }.min()
-
-	fun sumUnder(value: Int) = sizes().filter { it < value }.sum()
-
-	companion object {
-		fun ofCommands(commands: List<String>): Dir {
-			val path: MutableList<Dir> = mutableListOf(Dir("/", mutableListOf()))
-
-			for (cmd in commands) {
-				val parts = cmd.split(" ")
-				when (parts.first()) {
-					"$" -> handleCommand(path, parts)
-					else -> handleAddPart(path, parts)
-				}
-			}
-
-			return path.first()
-		}
-
-		private fun handleCommand(path: MutableList<Dir>, parts: List<String>) {
-			if (parts[1] == "ls") return
+	for (cmd in this) {
+		val parts = cmd.split(" ")
+		if (parts[0] == "$") {
+			if (parts[1] == "ls") continue
 			else if (parts[2] == "..") path.removeLast()
 			else if (parts[2] == "/") path.removeAll(path.drop(1))
 			else {
 				val cwd = path.last()
-				val nxt = cwd.childWithName(parts[2])
+				val nxt = cwd.entries.first { it.name == parts[2] }
 				path.add(nxt)
 			}
-		}
-
-		private fun handleAddPart(path: MutableList<Dir>, parts: List<String>) {
+		} else {
 			val cwd = path.last()
-			if (parts[0] == "dir") cwd.addDir(Dir(parts[1], mutableListOf()))
-			else cwd.addFile(parts[0].toInt())
+			if (parts[0] == "dir") cwd.entries.add(Dir.Builder(parts[1]))
+			else cwd.files += parts[0].toInt()
 		}
 	}
+
+	return path.first().build()
 }
 
 fun main() {
 	val commands = File("input.txt").readLines()
-	val tree = Dir.ofCommands(commands)
+	val tree = commands.buildTree()
 	val part1 = tree.sumUnder(100_000)
 	val neededSpace = 30_000_000 - (70_000_000 - tree.size)
 	val part2 = tree.minOver(neededSpace)
