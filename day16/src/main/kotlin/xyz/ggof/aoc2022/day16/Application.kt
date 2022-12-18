8042062cf93e51953cf937b46c63ceaea0bf0323
Gabriel Goffart <gab.goffart@gmail.com>
2022-12-18 12:16:49 -0500

    refactor for bitmasking + data class as key for hashmap, WAY faster (still slow)

--- day16/src/main/kotlin/xyz/ggof/aoc2022/day16/Application.kt
+++ day16/src/main/kotlin/xyz/ggof/aoc2022/day16/Application.kt
@@ -1,60 +1,76 @@
 package xyz.ggof.aoc2022.day16
 
-import kotlinx.collections.immutable.*
-import kotlinx.collections.immutable.plus
 import java.io.File
 import java.lang.Integer.max
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
-data class Valve(val rate: Int, val next: List<String>)
+data class Valve(val rate: Int, val next: List<Int>)
 
-typealias Cave = Map<String, Valve>
+typealias Cave = Array<Valve>
 
-fun String.toCaveEntry(): Pair<String, Valve> {
+fun String.toCaveEntry(): Pair<Int, Valve> {
 	val (f, s) = split(";").map { it.trim() }
-	val name = f.drop(6).take(2)
+	val name = f.drop(6).takeWhile { it.isDigit() }.toInt()
 	val rate = f.takeLastWhile { it.isDigit() }.toInt()
-	val neighbors = s.split(" ").drop(4).map { it.trimEnd(',') }
+	val neighbors = s.split(" ").drop(4).map { it.trimEnd(',').trim() }.map { it.toInt() }
 	return name to Valve(rate, neighbors)
 }
 
-class CachedSolver(val cave: Cave) {
-	private val cache = HashMap<List<Any>, Int>()
+val Int.mask: ULong get() = 1UL shl this
 
-	fun solve(pos: String, time: Int, opened: PersistentSet<String>, more: Boolean = false): Int {
-		val key = listOf(pos, time, opened, more)
-		if (key in cache) return cache[key]!!
+class CachedSolver(val cave: Cave, val start: Int) {
+	data class Key(val pos: Int, val time: Int, val opened: ULong, val more: Boolean)
+
+	private val cache = HashMap<Key, Int>(30_000_000)
+
+	fun solve(pos: Int, time: Int, opened: ULong, more: Boolean = false): Int {
+		val key = Key(pos, time, opened, more)
+		val v = cache[key]
+		if (v != null) return v
 		val result = solverec(pos, time, opened, more)
 		cache[key] = result
 		return result
 	}
 
-	fun solverec(pos: String, time: Int, opened: PersistentSet<String>, more: Boolean = false): Int {
+	fun solverec(pos: Int, time: Int, opened: ULong, more: Boolean = false): Int {
 		if (time == 0) {
-			if (more) return solve("AA", 26, opened)
-			return 0
+			return if (!more) 0
+			else solve(start, 26, opened)
 		}
 
-		val valve = cave[pos]!!
+		val valve = cave[pos]
 
 		val score = valve.next.maxOf { solve(it, time - 1, opened, more) }
 
-		return if (valve.rate == 0 || pos in opened) score
-		else max(score, (time - 1) * valve.rate + solve(pos, time - 1, opened + pos, more))
+		return if (valve.rate == 0 || pos.mask and opened != 0UL) score
+		else max(score, (time - 1) * valve.rate + solve(pos, time - 1, opened or pos.mask, more))
 	}
 }
 
-fun List<String>.toCave(): Cave =
-	map { it.toCaveEntry() }.fold(hashMapOf()) { acc, cur -> acc[cur.first] = cur.second; acc }
+fun List<String>.toCave(): Cave = map { it.toCaveEntry() }
+	.fold(Array<Valve?>(size) { null }) { acc, cur -> acc[cur.first] = cur.second; acc }
+	.requireNoNulls()
+
+fun String.replaceNames() = lines()
+	.map { it.split(" ").component2() }
+	.withIndex()
+	.fold(this) { acc, (i, name) -> acc.replace(name, i.toString()) }
+	.lines()
+
+fun String.startIndex() = lines().indexOfFirst { it.split(" ")[1] == "AA" }
 
 @OptIn(ExperimentalTime::class)
 fun main() {
-	val input = File("input.txt").readLines().toCave()
+	val text = File("input.txt").readText()
+	val aa = text.startIndex()
+	val input = text.replaceNames().toCave()
+	val solver = CachedSolver(input, aa)
 
-	val part1 = measureTimedValue { CachedSolver(input).solve("AA", 30, persistentHashSetOf()) }
+	val part1 = measureTimedValue { solver.solve(aa, 30, 0U) }
 	println("part 1: ${part1.value} in ${part1.duration}")
-	val part2 = measureTimedValue { CachedSolver(input).solve("AA", 26, persistentHashSetOf(), true) }
+	val part2 = measureTimedValue { solver.solve(aa, 26, 0U, true) }
 	println("part 2: ${part2.value} in ${part2.duration}")
 }
--- day16/src/test/kotlin/xyz/ggof/aoc2022/day16/ApplicationKtTest.kt
+++ day16/src/test/kotlin/xyz/ggof/aoc2022/day16/ApplicationKtTest.kt
@@ -1,12 +1,10 @@
 package xyz.ggof.aoc2022.day16
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.maps.shouldContainAll
 import io.kotest.matchers.shouldBe
-import xyz.ggof.aoc2022.day16.*
 
 class ApplicationKtTest : StringSpec({
-	val input = """
+	val text = """
 Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 Valve BB has flow rate=13; tunnels lead to valves CC, AA
 Valve CC has flow rate=2; tunnels lead to valves DD, BB
@@ -17,20 +15,20 @@ Valve GG has flow rate=0; tunnels lead to valves FF, HH
 Valve HH has flow rate=22; tunnel leads to valve GG
 Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II
-	""".trimIndent().lines()
+	""".trimIndent()
 
-	"should create a  map" {
-		input.toCave() shouldContainAll mapOf(
-			"AA" to Valve(0, listOf("DD", "II", "BB")),
-			"BB" to Valve(13, listOf("CC", "AA")),
-			"CC" to Valve(2, listOf("DD", "BB")),
-			"DD" to Valve(20, listOf("CC", "AA", "EE")),
-		)
+	"should find path with most pressure release" {
+		val first = text.startIndex()
+		val input = text.replaceNames().toCave()
+		val pr = CachedSolver(input, 30).solve(first, 30, 0UL)
+		pr shouldBe 1651
 	}
 
-	"should find every possible way" {
+	"should find double path with most pressure release" {
+		val first = text.startIndex()
+		val input = text.replaceNames().toCave()
+		val pr = CachedSolver(input, first).solve(first, 26, 0UL, true)
+		pr shouldBe 1707
 
-		val everyPath = CachedSolver(input.toCave()).solve("AA", 30, emptySet())
-		everyPath shouldBe 1651
 	}
 })
